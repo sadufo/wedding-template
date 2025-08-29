a64aab5f591a7b53fb7b2f6856cb5e2309a7a9bd
sadufo <maxzaplitnyy@gmail.com>
2025-08-30 02:38:36 +0300

    1

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: "/<repo>",
-  assetPrefix: "/<repo>/",
+  basePath: "/my-v0-project",
+  assetPrefix: "/my-v0-project/",
   // ...existing config...
 }
 
